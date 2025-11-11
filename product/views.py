6563b15efda70c356ecab59bcc1d49af4383a309
Arjun Mazumder <arjumazumder2@gmail.com>
2025-11-11 10:19:48 +0600

    m20.2

--- product/views.py
+++ product/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from product.models import Product, Category
 
 @api_view()
 def view_specific_product(request, id):
-    product = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id)
     product_dic = {
         'id' : product.id,
         'name' : product.name,
         'price' : product.price
     }
     return Response(product_dic)
+ 
     
