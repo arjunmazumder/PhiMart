347a96893d78461e6d2a76ae3501bea111d2d4ac
Arjun Mazumder <arjumazumder2@gmail.com>
2025-11-11 09:14:47 +0600

    m20.1

---
+++ product/product_urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from product import views
+
+urlpatterns = [
+    path('<int:id>/', views.view_specific_product, name='view-specific-product')
+
+]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from product.models import Product, Category
+
+@api_view()
+def view_specific_product(request, id):
+    product = Product.objects.get(pk=id)
+    product_dic = {
+        'id' : product.id,
+        'name' : product.name,
+        'price' : product.price
+    }
+    return Response(product_dic)
+    
