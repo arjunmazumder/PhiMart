27ff897e3c9dd5ead4dd2c36fc11f4c5e73831eb
Arjun Mazumder <arjumazumder2@gmail.com>
2025-11-12 21:09:19 +0600

    validate price implementation  done

--- product/serializers.py
+++ product/serializers.py
@@ -13,7 +13,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     price_with_tax = serializers.SerializerMethodField()
-    
+
     category = serializers.HyperlinkedRelatedField(
         queryset=Category.objects.all(),
         view_name='view-specific-category'
@@ -25,3 +25,9 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_price_with_tax(self, product):
         return round(product.price * Decimal(1.1), 2)
+    
+    def validate_price(self, price):
+
+        if price < 0:
+            raise serializers.ValidationError('Price could not be negative')
+        return price
