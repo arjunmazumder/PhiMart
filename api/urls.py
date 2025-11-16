8669aedfd7bb01a30f99d58a6de54dba12850b78
Arjun Mazumder <arjumazumder2@gmail.com>
2025-11-16 21:25:29 +0600

    module get cart items implementation done

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from product.views import ProductModelViewSet, CategoryModelViewSet, ReviewModelViewSet
-from order.views import CartViewSet
+from order.views import CartViewSet, CartItemViewSet
 from rest_framework_nested import routers
 
 
@@ -12,11 +12,15 @@ router.register('carts', CartViewSet, basename='carts')
 product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 product_router.register('reviews', ReviewModelViewSet, basename='product-review')
 
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register('items', CartItemViewSet, basename='cart-item')
+
 # urlpatterns = router.urls
     
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(product_router.urls))
+    path('', include(product_router.urls)),
+    path('', include(cart_router.urls))
     # path('products/', include('product.product_urls')),
     # path('categories/', include('product.category_urls')),   
 ]
--- order/views.py
+++ order/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from order.serializer import CartSerializer
-from rest_framework.viewsets import GenericViewSet
+from order.serializer import CartSerializer, CartItemSetializer
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
-from order.models import Cart
+from order.models import Cart, CartItem
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSetializer
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
     
